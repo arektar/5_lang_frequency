25e7724e0a40d113b0aff2e1c8b63796ecb215e3
arektar <zander5@mail.ru>
2017-01-17 15:10:33 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -4,8 +4,8 @@ import re
 
 def load_data(file_path):
     with open(file_path) as file:
-        words = re.findall(r'\w+', file.read().lower())
-    return words
+        text = file.read()
+    return text
 
 
 def get_most_frequent_words(words):
@@ -13,9 +13,14 @@ def get_most_frequent_words(words):
     return collections.Counter(words).most_common(lot_of_freq_words)
 
 
+def take_words(text):
+    words = re.findall(r'\w+', text.lower())
+    return words
+
 if __name__ == '__main__':
     my_file_name = input("Write full file way: ")
-    my_text_words = load_data(my_file_name)
+    my_text = load_data(my_file_name)
+    my_text_words = take_words(my_text)
     print("Топ часто встречающихся слов:")
     for fr_word, times_found in get_most_frequent_words(my_text_words):
         print("%s %d раз(а)." % (fr_word, times_found))
